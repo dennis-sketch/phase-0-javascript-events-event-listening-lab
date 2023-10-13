ddbeab2eb85fafd951ffac9fe6af36d71893d6bc
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-13 08:03:10 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+
+
 function addingEventListener() {
+  // Grab the element with id 'input'
+  const input = document.getElementById('input');
+
+  // Define a callback function
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  // Add an event listener for the click event
+  input.addEventListener('click', clickAlert);
 }
+
+// Call the function to set up the event listener
+addingEventListener();
